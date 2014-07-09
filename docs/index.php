a92ec519f4e4667c90525a6a82b5a490f02413e0
matto <matt@mirabot.coop>
2014-07-09 11:43:30 -0700

    Theming.

--- docs/index.php
+++ docs/index.php
@@ -51,66 +51,64 @@ elseif (isset($_GET['email-submit'])) {
     <script src="js/truisms.js" type="text/javascript"></script>
   </head>
   <body>
-    <header>
-      <h1><a href="index.php">Truisms</a></h1>
-    </header>
-    <section id="about">
-      <h2>About</h2>
-      <p>
-        This site allows you to signup for daily text message or emails to
-        receive a (machine) selected quote from 
-        <a href="https://en.wikipedia.org/wiki/Jenny_Holzer">Jenny Holzer's</a> 
-        original truisms series. This site makes no claims of ownership over the
-        work, makes no financial gain from the work and seeks only to breathe 
-        life into through a new medium. I hope you enjoy this work as I have.
-      </p>
-      <p>
-        Dedicated to M.S.R., E.R.C and B.L. for their mutual appreciation of
-        Holzer's work, their friendship and support.
-      </p>
-    </section>
-    <section id="signup">
-      <h2>Signup</h2>
-      <?php if (isset($message)) { echo $message; } ?>
-      <div id="help-text">
-        Please submit a phone number or email address to subscribe:
-      </div>
-      <div id="signup-type"></div>
-      <form id="signup-phone">
-        <p>Enter your phone number, including area code:</p>
-        <input name="phone" type="text" placeholder="Phone Number" required />
-        <select name="provider" required>
-          <option value="message.alltel.com">Alltel</option>
-	        <option value="txt.att.net">AT&T</option>
-          <option value="myboostmobile.com">Boost">Mobile</option>
-          <option value="sms.mycricket.com">Cricket</option>
-          <option value="mymetropcs.com">Metro PCS</option>
-          <option value="messaging.nextel.com">Nextel</option>
-          <option value="ptel.com">Ptel</option>
-	        <option value="qwestmp.com">Qwest</option>
-          <option value="messaging.sprintpcs.com">Sprint</option>
-          <option value="tms.suncom.com">Suncom</option>
-	        <option value="tmomail.net">T-Mobile</option>
-      	  <option value="mmst5.tracfone.com">Tracfone</option>
-	        <option value="email.uscc.net">U.S. Cellular</option>
-	        <option value="vtext.com">Verizon</option>
-          <option value="vmobl.com">Virgin Mobile</option>
-        </select>
-        <input name="phone-submit" type="submit" value="Submit"/>
-      </form>
-      <noscript><h2>or</h2></noscript>
-      <form id="signup-email">
-        <p>Enter your email address:</p>
-        <input name="email" type="text" placeholder="Email Address" required />
-        <input name="email-submit" type="submit" value="Submit"/>
-      </form>
-    </section>
-    <section id="unsubscribe">
-      <a href="unsubscribe.php">Unsubscribe</a>
-    </section>
-    <section id="privacy-policy">
-      <h2>Privacy Policy</h2>
-      This site stores your supplied email and/or phone information to send you messages. This information is not shared with any third parties.
-    </section>
+    <div id="wrapper">
+      <header>
+        <h1><a href="index.php">Truisms</a></h1>
+      </header>
+      <section id="about">
+        <h2>About</h2>
+        <p>
+          This site allows you to signup for daily text message or emails to
+          receive a (machine) selected quote from 
+          <a href="https://en.wikipedia.org/wiki/Jenny_Holzer">Jenny Holzer's</a> 
+          original truisms series. This site makes no claims of ownership over the
+          work, makes no financial gain from the work and seeks only to explore
+          the content of the work in a new medium.
+        </p>
+      </section>
+      <section id="signup">
+        <h2>Signup</h2>
+        <?php if (isset($message)) { echo $message; } ?>
+        <div id="help-text">
+          Please enter your phone number or email address to subscribe:
+        </div>
+        <div id="signup-type"></div>
+        <form id="signup-phone">
+          <p>Enter your <strong>phone number</strong>, including area code:</p>
+          <input name="phone" type="text" placeholder="Phone Number" required />
+          <select name="provider" required>
+            <option value="message.alltel.com">Alltel</option>
+	          <option value="txt.att.net">AT&T</option>
+            <option value="myboostmobile.com">Boost">Mobile</option>
+            <option value="sms.mycricket.com">Cricket</option>
+            <option value="mymetropcs.com">Metro PCS</option>
+            <option value="messaging.nextel.com">Nextel</option>
+            <option value="ptel.com">Ptel</option>
+  	        <option value="qwestmp.com">Qwest</option>
+            <option value="messaging.sprintpcs.com">Sprint</option>
+            <option value="tms.suncom.com">Suncom</option>
+	          <option value="tmomail.net">T-Mobile</option>
+      	    <option value="mmst5.tracfone.com">Tracfone</option>
+	          <option value="email.uscc.net">U.S. Cellular</option>
+	          <option value="vtext.com">Verizon</option>
+            <option value="vmobl.com">Virgin Mobile</option>
+          </select>
+          <input name="phone-submit" type="submit" value="Submit"/>
+        </form>
+        <noscript><h2>or</h2></noscript>
+        <form id="signup-email">
+          <p>Enter your <strong>email address</strong>:</p>
+          <input name="email" type="text" placeholder="Email Address" required />
+          <input name="email-submit" type="submit" value="Submit"/>
+        </form>
+      </section>
+      <section id="unsubscribe">
+        <a href="unsubscribe.php">Unsubscribe</a>
+      </section>
+      <section id="privacy-policy">
+        <h2>Privacy Policy</h2>
+        <p>This site stores your supplied email and/or phone information to send you messages. This information is not shared with any third parties.</p>
+      </section>
+    </div>
   </body>
 </html>
--- docs/unsubscribe.php
+++ docs/unsubscribe.php
@@ -34,7 +34,7 @@ elseif (isset($_GET['email-submit'])) {
 <!DOCTYPE html>
 <html>
   <head>
-    <title>TRUISMS</title>
+    <title>Unsubscribe | Truisms</title>
     <!-- CSS Here -->
     <link rel="stylesheet" href="css/styles.css" type="text/css" media="screen" />
     <!-- Scripts Here --> 
@@ -42,48 +42,50 @@ elseif (isset($_GET['email-submit'])) {
     <script src="js/truisms.js" type="text/javascript"></script>
   </head>
   <body>
-    <header>
-      <h1>Unsubscribe from <a href="index.php">Truisms</a></h1>
-    </header>
-    <section id="body">
-      <?php if (isset($message)) { echo $message; } ?>
-      <div id="help-text">
-        Please submit a phone number or email address to unsubscribe:
-      </div>
-
-      <div id="unsubscribe-type"></div>
-      <form id="unsubscribe-phone">
-        Enter your phone number, including area code:<br />
-        <input name="phone" type="text" placeholder="Phone Number" required />
-        <select name="provider" required>
-          <option value="message.alltel.com">Alltel</option>
-	        <option value="txt.att.net">AT&T</option>
-          <option value="myboostmobile.com">Boost">Mobile</option>
-          <option value="sms.mycricket.com">Cricket</option>
-          <option value="mymetropcs.com">Metro PCS</option>
-          <option value="messaging.nextel.com">Nextel</option>
-          <option value="ptel.com">Ptel</option>
-	        <option value="qwestmp.com">Qwest</option>
-          <option value="messaging.sprintpcs.com">Sprint</option>
-          <option value="tms.suncom.com">Suncom</option>
-	        <option value="tmomail.net">T-Mobile</option>
-      	  <option value="mmst5.tracfone.com">Tracfone</option>
-	        <option value="email.uscc.net">U.S. Cellular</option>
-	        <option value="vtext.com">Verizon</option>
-          <option value="vmobl.com">Virgin Mobile</option>
-        </select>
-        <input name="phone-submit" type="submit" value="Submit"/>
-      </form>
-      <noscript><h2>or</h2></noscript>
-      <form id="unsubscribe-email">
-        Enter your email address:<br />
-        <input name="email" type="text" placeholder="Email Address" required />
-        <input name="email-submit" type="submit" value="Submit"/>
-      </form>
-    </section>
-    <section id="privacy-policy">
-      <h2>Privacy Policy</h2>
-      This site stores your supplied email and/or phone information to send you messages. This information is not shared with any third parties.
-    </section>
+    <div id="wrapper">
+      <header>
+        <h1><a href="index.php">Truisms</a></h1>
+      </header>
+      <section id="unsubscribe">
+        <h2>Unsubscribe from Truisms</h2>
+        <?php if (isset($message)) { echo $message; } ?>
+        <div id="help-text">
+          Please enter your phone number or email address to unsubscribe:
+        </div>
+        <div id="unsubscribe-type"></div>
+        <form id="unsubscribe-phone">
+          <p>Enter your phone number, including area code:</p>
+          <input name="phone" type="text" placeholder="Phone Number" required />
+          <select name="provider" required>
+            <option value="message.alltel.com">Alltel</option>
+	          <option value="txt.att.net">AT&T</option>
+            <option value="myboostmobile.com">Boost">Mobile</option>
+            <option value="sms.mycricket.com">Cricket</option>
+            <option value="mymetropcs.com">Metro PCS</option>
+            <option value="messaging.nextel.com">Nextel</option>
+            <option value="ptel.com">Ptel</option>
+  	        <option value="qwestmp.com">Qwest</option>
+            <option value="messaging.sprintpcs.com">Sprint</option>
+            <option value="tms.suncom.com">Suncom</option>
+	          <option value="tmomail.net">T-Mobile</option>
+      	    <option value="mmst5.tracfone.com">Tracfone</option>
+	          <option value="email.uscc.net">U.S. Cellular</option>
+	          <option value="vtext.com">Verizon</option>
+            <option value="vmobl.com">Virgin Mobile</option>
+          </select>
+          <input name="phone-submit" type="submit" value="Submit"/>
+        </form>
+        <noscript><h2>or</h2></noscript>
+        <form id="unsubscribe-email">
+          <p>Enter your email address:</p>
+          <input name="email" type="text" placeholder="Email Address" required />
+          <input name="email-submit" type="submit" value="Submit"/>
+        </form>
+      </section>
+      <section id="privacy-policy">
+        <h2>Privacy Policy</h2>
+        <p>This site stores your supplied email and/or phone information to send you messages. This information is not shared with any third parties.</p>
+      </section>
+    </div>
   </body>
 </html>
