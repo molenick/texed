f78d78fe925e881dbecbe3c0ef2d47b04448c808
Matt Olenick <matt.olenick@gmail.com>
2015-08-04 07:34:23 -0700

    Added  variables for from and url to settings.php.

--- includes/default.settings.php
+++ includes/default.settings.php
@@ -2,8 +2,11 @@
 $conf = array();
   $conf['app_name'] = 'Texed';
   $conf['app_about'] = '<p>Texed is a PHP application for sending out blasts of predefined messages to users.</p>';
+  $conf['app_from'] = 'texed@example.org';
+  $conf['app_url'] = 'texed.example.org'; // No trailing slash.
   $conf['db'] = '';
   $conf['db_host'] = 'localhost';
   $conf['db_user'] = '';
   $conf['db_pass'] = '';
+  // @todo Add port.
 ?>
