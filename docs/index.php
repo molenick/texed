b6dbcfbc7ad95bd4b061278a0644ed964afa42d6
Matt Olenick <matt.olenick@gmail.com>
2015-08-04 18:40:30 -0700

    Replaced a few tabs with spaces.

--- docs/index.php
+++ docs/index.php
@@ -73,19 +73,19 @@ elseif (isset($_GET['email-submit'])) {
           <input name="phone" type="text" placeholder="Phone Number" required />
           <select name="provider" required>
             <option value="message.alltel.com">Alltel</option>
-	          <option value="txt.att.net">AT&T</option>
+            <option value="txt.att.net">AT&T</option>
             <option value="myboostmobile.com">Boost Mobile</option>
             <option value="sms.mycricket.com">Cricket</option>
             <option value="mymetropcs.com">Metro PCS</option>
             <option value="messaging.nextel.com">Nextel</option>
             <option value="ptel.com">Ptel</option>
-  	        <option value="qwestmp.com">Qwest</option>
+            <option value="qwestmp.com">Qwest</option>
             <option value="messaging.sprintpcs.com">Sprint</option>
             <option value="tms.suncom.com">Suncom</option>
-	          <option value="tmomail.net">T-Mobile</option>
-      	    <option value="mmst5.tracfone.com">Tracfone</option>
-	          <option value="email.uscc.net">U.S. Cellular</option>
-	          <option value="vtext.com">Verizon</option>
+            <option value="tmomail.net">T-Mobile</option>
+            <option value="mmst5.tracfone.com">Tracfone</option>
+            <option value="email.uscc.net">U.S. Cellular</option>
+            <option value="vtext.com">Verizon</option>
             <option value="vmobl.com">Virgin Mobile</option>
           </select>
           <input name="phone-submit" type="submit" value="Submit"/>
